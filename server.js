572734b32c0c917b66d69fdb565556e57aca2552
Akhil Morampudi <akhil@Akhils-MacBook-Pro.local>
2020-02-28 02:23:40 -0500

    modifying the requirement

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ app.use(cors());
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res){
+    const data = axios.get('https://ekmbusljl2.execute-api.us-east-1.amazonaws.com/prod')
+    .then((response) => {
+        res.send(response.data);
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
